fdfcf55de1a91cabdf458cf032610488a5072715
Navya <88023981+navysai1419@users.noreply.github.com>
2024-02-12 23:05:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-// Complete the js code
 function Car(make, model) {
-    constructor(make:string, model:string)
-	{
-		this.make=make;
-		this.model=model;
-	}
-	getMakeModel(){
-		return this.make , this.model;
-	}
-
-	
+    this.make = make;
+    this.model = model;
 }
+
+Car.prototype.getMakeModel = function() {
+    return this.make + ' ' + this.model;
+};
 
 function SportsCar(make, model, topSpeed) {
-    constructor(make: string, model: string, topSpeed: number){
-		this.make=make;
-		this.model=model;
-		this.toSpeed=topSpeed;
-	}
-	getTopSpedd(){
-		return this.speed;
-	}
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
 }
+
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
-
-
